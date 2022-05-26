5207902852d1596dd0817931e32fff70bb7e9437
IL KWON KIM <magictical@gmail.com>
2022-05-26 15:44:04 +0900

    addNavbar and habit counter

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,71 @@
+import React, { Component } from "react";
 import "./app.css";
-import Habits from "./components/habits";
+import AddHabit from "./components/addHabit";
 
-function App() {
-    return <Habits />;
+import Habits from "./components/habits";
+import Navbar from "./components/navbar";
+import Reset from "./components/reset";
+
+class App extends Component {
+    state = {
+        habits: [
+            { id: 1, name: "Reading", count: 0 },
+            { id: 2, name: "Running", count: 0 },
+            { id: 3, name: "Coding", count: 0 },
+        ],
+    };
+
+    handleIncrement = (habit) => {
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        habits[index].count++;
+        this.setState({ habits });
+    };
+    handleDecrement = (habit) => {
+        const habits = [...this.state.habits];
+        const index = habits.indexOf(habit);
+        const count = habits[index].count - 1;
+        habits[index].count = count < 0 ? 0 : count;
+
+        // 아래와 위는 동일 결과
+        // habits[index].count < 0
+        //     ? (habits[index].count = 0)
+        //     : habits[index].count--;
+
+        this.setState({ habits });
+    };
+    handleDelete = (habit) => {
+        const habits = this.state.habits.filter((item) => item.id !== habit.id);
+        this.setState({ habits });
+    };
+
+    // add habit
+    addHabit = (name) => {
+        const index = this.state.habits.length;
+        const habit = { id: index, name: name, count: 0 };
+        const habits = [...this.state.habits].push(habit);
+        this.setState({ habits });
+    };
+    render() {
+        return (
+            <>
+                <Navbar
+                    totalCount={
+                        this.state.habits.filter((item) => item.count > 0)
+                            .length
+                    }
+                />
+                <AddHabit OnAddHabit={this.addHabit} />
+                <Habits
+                    habits={this.state.habits}
+                    OnIncrement={this.handleIncrement}
+                    OnDecrement={this.handleDecrement}
+                    OnDelete={this.handleDelete}
+                />
+                <Reset />
+            </>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/reset.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Reset extends Component {
+    render() {
+        return (
+            <div>
+                <button>Reset All</button>
+            </div>
+        );
+    }
+}
+
+export default Reset;
+
+<button>Reset All</button>;
